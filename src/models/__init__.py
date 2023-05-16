17b1a24f9575b27fce527e846997fc696c883eb3
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-05-16 05:26:04 +0100

    update: added status to game play

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,4 +4,4 @@ db = SQLAlchemy()
 
 from .session import Session
 from .bankroll import BankRoll
-from .game import Game
+from .gameplay import GamePlay
--- src/models/gameplay.py
+++ src/models/gameplay.py
@@ -2,14 +2,15 @@ from datetime import datetime
 from models import db
 
 
-class Game(db.Model):
+class GamePlay(db.Model):
     """ The model defines the financial management of the user """
-    __tablename__ = "games"
+    __tablename__ = "gameplays"
 
     id = db.Column(db.Integer, primary_key=True)
     stake = db.Column(db.Float)
     target = db.Column(db.Float)
     odd = db.Column(db.Float)
+    status = db.Column(db.String())
 
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(
